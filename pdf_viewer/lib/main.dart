3ce2d4d29c12f633259a0e6fbc1c2acdc40d18c2
3li-3bdullah <PROGRMMER7@gmail.com>
2022-11-19 13:31:43 +0200

    I have used GetMaterail bcz i use the lovable state mangement GetX 😍

--- pdf_viewer/lib/main.dart
+++ pdf_viewer/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:pdf_viewer/view/home.dart';
 
 void main() {
@@ -10,12 +11,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const GetMaterialApp(
       title: 'PDF Viewer',
-      theme: ThemeData(
-        primarySwatch: MaterialColor(1, {1: Colors.deepPurple.shade100}),
-      ),
-      home: const Home(),
+      debugShowCheckedModeBanner: false,
+      home: Home(),
     );
   }
 }
