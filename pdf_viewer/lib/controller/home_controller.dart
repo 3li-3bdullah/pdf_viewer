74663c07793022708400f179d19d700e0dfc85d5
3li-3bdullah <PROGRMMER7@gmail.com>
2022-11-19 12:03:12 +0200

    Declared new vars and i have made the methods void

--- pdf_viewer/lib/controller/home_controller.dart
+++ pdf_viewer/lib/controller/home_controller.dart
@@ -4,19 +4,23 @@ import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
 
-/// The App Is Simple.😉., So I Don't Wanna Make File constants For Just This Line..
+/// The App Is Simple.😉.,
+///  So I Don't Wanna Make File constants For Just This Line, Got It ?..
 const String pdfUrl = "https://www.orimi.com/pdf-test.pdf";
 
 class HomeController extends GetxController {
+  /// .............. Declaring Variables ..😋.😋.😋................
+  RxString? filePath;
 
+  
   /// .............. Declaring Methods ..🤩.🤩.🤩..................
-  Future<String> fetchPDF() async {
+  Future<void> fetchPDF() async {
     http.Response response = await http.get(Uri.parse(pdfUrl));
 
     Directory dir = await getTemporaryDirectory();
     File file = File("${dir.path}/data.pdf");
 
     await file.writeAsBytes(response.bodyBytes, flush: true);
-    return file.path;
+    filePath!.value = file.path;
   }
 }
