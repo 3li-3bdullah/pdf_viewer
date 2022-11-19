61a2f0d9c92595f926d7cd41ce961edda8eaafc8
3li-3bdullah <PROGRMMER7@gmail.com>
2022-11-19 13:30:38 +0200

    I have created new class

---
+++ pdf_viewer/lib/view/pdf_viewer.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class PDFViewer extends StatelessWidget {
+  const PDFViewer({super.key, required this.filepath});
+  final String filepath;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
