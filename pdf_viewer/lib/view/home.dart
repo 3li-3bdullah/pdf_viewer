a018166dacd4b9840fe0243e0ebe937b0fb89c75
3li-3bdullah <PROGRMMER7@gmail.com>
2022-11-19 12:04:49 +0200

    Added some widgets

--- pdf_viewer/lib/view/home.dart
+++ pdf_viewer/lib/view/home.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
+import 'package:get/get.dart';
+import 'package:pdf_viewer/controller/home_controller.dart';
 
-class Home extends StatelessWidget {
+class Home extends GetWidget<HomeController> {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("PDF Viewer 😍"),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple.shade100,
+      ),
+      body: PDFView(
+        filePath: controller.filePath!.value,
+      ),
+    );
   }
 }
